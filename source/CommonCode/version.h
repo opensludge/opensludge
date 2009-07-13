d82d1b1dbba7f3ffe5d37283a7ed21143f2ee162
trumgottist <trumgottist@27a91dd4-ec73-4ff0-838b-64dfa05f697e>
2009-07-13 15:41:49 +0000

    Documentation fix: Removed all references to the registered development kit. SLUDGE is freeware now!
    git-svn-id: svn://svn.code.sf.net/p/opensludge/code@5 27a91dd4-ec73-4ff0-838b-64dfa05f697e

--- source/CommonCode/version.h
+++ source/CommonCode/version.h
@@ -4,6 +4,6 @@
 #define WHOLE_VERSION (MAJOR_VERSION * 256 + MINOR_VERSION)
 #define MINIM_VERSION (1 			 * 256 + 2)
 
-#define COPYRIGHT_TEXT "\251 Hungry Software 2000-2009"
+#define COPYRIGHT_TEXT "\251 Hungry Software and contributors 2000-2009"
 
 #define VERSION(a,b)	(a * 256 + b)
