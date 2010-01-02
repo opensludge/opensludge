4f0af82d94bf345a2dc54c52ee56c9b88f605b76
trumgottist <trumgottist@27a91dd4-ec73-4ff0-838b-64dfa05f697e>
2010-01-02 11:06:22 +0000

    Dev Kit: Added saving / loading of preferences.
    git-svn-id: svn://svn.code.sf.net/p/opensludge/code@63 27a91dd4-ec73-4ff0-838b-64dfa05f697e

--- Mac Dev Kit/AppController.m
+++ Mac Dev Kit/AppController.m
@@ -30,6 +30,7 @@ AppController *aC;
 	[prefKeepImages setState: ! programSettings.compilerKillImages];
 	[prefWriteStrings setState: programSettings.compilerWriteStrings];
 	[prefVerbose setState: programSettings.compilerVerbose];
+	
 }
 
 - (IBAction)menuOpen:(id)sender
@@ -189,20 +190,40 @@ bail: return err;
 
 }
 
-
-
+void saveIniFile() {
+	unsigned char appsupport_path[1024];
+	FSRef foundRef;
+	
+	// Find the preference folder and go there
+	FSFindFolder (kUserDomain, kPreferencesFolderType , kDontCreateFolder, &foundRef);	
+	FSRefMakePath( &foundRef, appsupport_path, 1024);
+	chdir ((char *) appsupport_path);
+	mkdir ("SLUDGE Development Kit", 0000777);
+	chdir ("SLUDGE Development Kit");
+	
+	FILE * fp = fopen ("SLUDGE.ini", "wb");
+	
+	fprintf (fp, "KillImages=%d\n", programSettings.compilerKillImages);
+	fprintf (fp, "WriteStrings=%d\n", programSettings.compilerWriteStrings);
+	fprintf (fp, "Verbose=%d\n", programSettings.compilerVerbose);
+	fprintf (fp, "SearchSensitive=%d\n", programSettings.searchSensitive);
+	fclose (fp);
+}
 
 - (IBAction)prefKeepImagesClick:(id)sender
 {
 	programSettings.compilerKillImages = ! [prefKeepImages state];
+	saveIniFile();
 }
 - (IBAction)prefWriteStringsClick:(id)sender
 {
 	programSettings.compilerWriteStrings = [prefWriteStrings state];
+	saveIniFile();
 }
 - (IBAction)prefVerboseClick:(id)sender
 {
 	programSettings.compilerVerbose = [prefVerbose state];
+	saveIniFile();
 }
 
 
@@ -224,7 +245,111 @@ bool errorBox (const char * head, const char * msg) {
 	return false;
 }
 
+unsigned int stringToInt (char * s) {
+	int i = 0;
+	for (;;) {
+		if (*s >= '0' && *s <= '9') {
+			i *= 10;
+			i += *s - '0';
+			s ++;
+		} else {
+			return i;
+		}
+	}
+}
+
+void readIniFile () {
+	unsigned char appsupport_path[1024];
+	FSRef foundRef;
+
+	// Find the preference folder and go there
+	FSFindFolder (kUserDomain, kPreferencesFolderType , kDontCreateFolder, &foundRef);	
+	FSRefMakePath( &foundRef, appsupport_path, 1024);
+	chdir ((char *) appsupport_path);
+	mkdir ("SLUDGE Development Kit", 0000777);
+	chdir ("SLUDGE Development Kit");
+
+	FILE * fp = fopen ("SLUDGE.ini", "rb");
+	
+	programSettings.compilerKillImages = 0;
+	programSettings.compilerWriteStrings = 0;
+	programSettings.compilerVerbose = 1;
+	programSettings.searchSensitive = 0;
+		
+	if (fp) {
+		char lineSoFar[257] = "";
+		char secondSoFar[257] = "";
+		unsigned char here = 0;
+		char readChar = ' ';
+		bool keepGoing = true;
+		bool doingSecond = false;
+		
+		do {
+			readChar = fgetc (fp);
+			if (feof (fp)) {
+				readChar = '\n';
+				keepGoing = false;
+			}
+			switch (readChar) {
+				case '\n':
+				case '\r':
+
+					fprintf (fp, "KillImages=%d\n", programSettings.compilerKillImages);
+					fprintf (fp, "WriteStrings=%d\n", programSettings.compilerWriteStrings);
+					fprintf (fp, "Verbose=%d\n", programSettings.compilerVerbose);
+					fprintf (fp, "SearchSensitive=%d\n", programSettings.searchSensitive);
+					
+					
+					if (doingSecond) {
+						if (strcmp (lineSoFar, "KillImages") == 0)
+						{
+							programSettings.compilerKillImages = stringToInt (secondSoFar);
+						}
+						else if (strcmp (lineSoFar, "WriteStrings") == 0)
+						{
+							programSettings.compilerWriteStrings = stringToInt (secondSoFar);
+						}
+						else if (strcmp (lineSoFar, "Verbose") == 0)
+						{
+							programSettings.compilerVerbose = stringToInt (secondSoFar);
+						}
+						else if (strcmp (lineSoFar, "SearchSensitive") == 0)
+						{
+							programSettings.searchSensitive = stringToInt (secondSoFar);
+						}
+					}
+					here = 0;
+					doingSecond = false;
+					lineSoFar[0] = 0;
+					secondSoFar[0] = 0;
+					break;
+					
+				case '=':
+					doingSecond = true;
+					here = 0;
+					break;
+					
+				default:
+					if (doingSecond) {
+						secondSoFar[here ++] = readChar;
+						secondSoFar[here] = 0;
+					} else {
+						lineSoFar[here ++] = readChar;
+						lineSoFar[here] = 0;
+					}
+					break;
+			}
+		} while (keepGoing);
+		
+		fclose (fp);
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
+	
+	readIniFile ();
+	
 	return NSApplicationMain (argc, (const char **) argv);
 }
