72775087fbcc30dbbdd99e8e521a80ededfd2415
trumgottist <trumgottist@27a91dd4-ec73-4ff0-838b-64dfa05f697e>
2010-12-28 21:34:59 +0000

    A bit of cleanup, removing old icons and a few unused source files. (Also discovered that the Mac Dev Kit was still using the old icon for SLUDGE scripts. This is now fixed.)
    git-svn-id: svn://svn.code.sf.net/p/opensludge/code@349 27a91dd4-ec73-4ff0-838b-64dfa05f697e

--- source/Translator/wincomp.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WIN32
-#define HWND int
-#endif
-
-#define MESS(id,m,w,l) SendDlgItemMessage(mainWin,id,m,(WPARAM)w,(LPARAM)l)
-
-char * getFileFromBox (int i);
-void drawLogo (HWND hDlg, int pic, int x, int y);
