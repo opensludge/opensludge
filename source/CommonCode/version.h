4e5306fa403c8971a540a59217c7f92cb51f8e55
trumgottist <trumgottist@27a91dd4-ec73-4ff0-838b-64dfa05f697e>
2011-03-13 14:09:53 +0000

    Windows Engine: Updated version number to 2.1.
    git-svn-id: svn://svn.code.sf.net/p/opensludge/code@449 27a91dd4-ec73-4ff0-838b-64dfa05f697e

--- source/CommonCode/version.h
+++ source/CommonCode/version.h
@@ -1,9 +1,9 @@
 #define MAJOR_VERSION 2
 #define MINOR_VERSION 1
-#define BUILD_VERSION 202
+#define BUILD_VERSION 203
 #define TEXT_VERSION "2.1"
 #define WHOLE_VERSION (MAJOR_VERSION * 256 + MINOR_VERSION)	// This version
-#define MINIM_VERSION (1 			 * 256 + 2)				// Earliest version of games the engine can run 
+#define MINIM_VERSION (1 			 * 256 + 2)				// Earliest version of games the engine can run
 
 #define COPYRIGHT_TEXT "\251 Hungry Software and contributors 2000-2011"
 
