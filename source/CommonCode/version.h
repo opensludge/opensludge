c853d9ad64edcf0723a9309c4b0055e28a0ab113
trumgottist <trumgottist@27a91dd4-ec73-4ff0-838b-64dfa05f697e>
2011-04-02 12:55:37 +0000

    Upped the build number, and changed the engine's RC file to use the version numbers defined in version.h.
    git-svn-id: svn://svn.code.sf.net/p/opensludge/code@456 27a91dd4-ec73-4ff0-838b-64dfa05f697e

--- source/CommonCode/version.h
+++ source/CommonCode/version.h
@@ -1,6 +1,6 @@
 #define MAJOR_VERSION 2
 #define MINOR_VERSION 1
-#define BUILD_VERSION 204
+#define BUILD_VERSION 205
 #define TEXT_VERSION "2.1"
 #define WHOLE_VERSION (MAJOR_VERSION * 256 + MINOR_VERSION)	// This version
 #define MINIM_VERSION (1 			 * 256 + 2)				// Earliest version of games the engine can run
